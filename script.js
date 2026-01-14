6e3aae7139cefbed64a0c272fc22fcc86cc7b0cc
ungratefulSnail <dragosbirsan92@gmail.com>
2026-01-14 07:59:14 +0200

    Refactor JS, new function for creating squares in the container

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+// Select the container and button
 const container = document.querySelector('.container');
+const newGridBtn = document.querySelector('#newGridBtn');
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    container.appendChild(div);
-    div.classList.add('div');
+// Sketch pad measurements total in pixels
+const CONTAINER_SIZE = 560;
+
+// Starting size
+let currentSize = 16;
+
+
+function createGrid(size) {
+    // Remove everything from container
+    container.innerHTML = '';
+
+    // Calculate each square size
+    squareSize = CONTAINER_SIZE / size;
+
+    // Create the total square numbers (size * size)
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+
+        // Set the size in pixels
+        square.style.width = squareSize + 'px';
+        square.style.height = squareSize + 'px';
+    }
+
+    // Adds the squares in the container
+    container.appendChild(square);
 }
